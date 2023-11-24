cc2fcbf3288c54eeb90065b0299f76fdf94b23ff
sauravi28 <100774982+sauravi28@users.noreply.github.com>
2023-11-24 18:52:07 +0530

    Add files via upload

---
+++ HAloocom_CRM_HC_casetype_and sent_attachmentto client/case_close_email_with_attachment.php
@@ -0,0 +1,88 @@
+<?php
+
+		$hostname='localhost';
+		$user = 'root';
+		$password = 'Hal0o(0m@72427242';
+		$mysql_database = 'haloocomCRM';
+		$link = mysqli_connect($hostname, $user, $password,$mysql_database);
+		date_default_timezone_set('UTC');
+
+
+	$date = date("Y-m-d");
+	$sql_ticket = "select cc.client_email_id_c, cc.email_update_c,c.id,c.name,c.case_number,c.date_entered,c.date_modified,c.modified_user_id,c.state,cc.case_type_c from cases c left join cases_cstm cc on c.id = cc.id_c where c.date_modified like '$date%' and c.state in ('Closed','Resolved') and cc.case_type_c='Health_Checkup' and c.filename!='' and cc.email_hc_attachment_sent_c='0'";		 //assigned_user_id //order by date_modified DESC limit 1
+	//echo $sql_ticket;
+	//die;
+	$result_ticket = mysqli_query($link,$sql_ticket);
+	while($row_ticket = mysqli_fetch_array($result_ticket)) {
+
+		$ticketName = $row_ticket['name'];
+		$ticketId = $row_ticket['id'];
+		$case_number = $row_ticket['case_number'];
+		$date_entered = $row_ticket['date_entered'];
+		$date_modified = $row_ticket['date_modified'];
+		$assigned_userID = $row_ticket['modified_user_id'];
+		$client_email_id = $row_ticket['client_email_id_c'];
+		$case_type_c = $row_ticket['case_type_c'];
+
+
+		$sql_user = "SELECT user_name FROM `users` where id = '".$assigned_userID."'";
+		$result_user = mysqli_query($link,$sql_user);
+		$row_user = mysqli_fetch_array($result_user);
+		$ticketName = $row_user['user_name'];
+		$assigned_user_id = $ticketName;
+ 
+ 
+		require_once "/var/www/html/Mail.php"; // PEAR Mail package
+		require_once "/var/www/html/Mail/mime.php"; // PEAR Mail_Mime packge
+		
+				$from = "<crm@haloocom.com>";  //haloopri@gmail.com
+				$to = $client_email_id.",<support@haloocom.com>";
+				$subject = "Haloocom Support Ticket - ".$case_number ." and Case type -".$case_type_c." Closed."; 
+				$headers = array ('From' => $from,'To' => $to, 'Subject' => $subject);
+				$text = " Dear Customer,
+I'm pleased to inform you that your Haloocom Server's health check-up was successfully completed today. Attached, you'll find the detailed report summarizing the evaluation.
+Should you have any questions about the report or need further assistance, feel free to reach out.
+
+Note : Download the file and rename is as .pdf format.
+
+Regards,
+Haloocom Support
+You can reach us on our 24 x 7 
+Help Desk Line : +91 80 35510099
+Email : helpdesk@haloocom.com; support@haloocom.com" ; // text and html versions of email.
+				$crlf = "\n";
+				$mime = new Mail_mime($crlf);
+				$mime->setTXTBody($text);
+				//$mime->setHTMLBody($html);
+				if($case_type_c == "Health_Checkup"){
+					$file ="upload/".$ticketId;
+					//$fileWithExt = $file.".pdf";
+					//echo $fileWithExt;
+					//die;
+					$mime->addAttachment($file, 'application/octet-stream');
+				}
+				$body = $mime->get();
+				$headers = $mime->headers($headers);
+
+				$host = "smtp.gmail.com"; 
+				$username = "crm@haloocom.com"; 
+				$password = "zggfhiqxqbmhzcou";  
+				$port ="587";  
+
+				$smtp = Mail::factory('smtp', array ('host' => $host,'port'=>$port, 'auth' => true, 'username' =>$username,'password' => $password));
+				$mail = $smtp->send($to, $headers, $body);
+					if (PEAR::isError($mail))
+						{
+							echo("<p>" . $mail->getMessage() . "</p>");
+						}
+					else
+						{
+							echo("Email successfully sent!");
+							
+							$update_emailUpdate = 'update cases_cstm set email_hc_attachment_sent_c="1"  where id_c="'.$ticketId.'"'; 
+							mysqli_query($link,$update_emailUpdate);
+							
+						}								
+
+}
+?>
---
+++ HAloocom_CRM_HC_casetype_and sent_attachmentto client/validationform.js
@@ -0,0 +1,367 @@
+$(document).ready(function(){
+	//alert('inside');
+	document.getElementById("tat_time_c").readOnly = true;
+	
+	var case_type_value = document.getElementById("case_type_c").value;
+	
+    if(case_type_value == 'Health_Checkup'){
+		
+        document.getElementById("filename_new").style.display = 'block';
+
+    }
+    else {
+        document.getElementById("filename_new").style.display = 'none';
+    }
+	
+});
+
+function getcheckContactsName(){
+var name,
+element = document.getElementById('account_name');
+if (element != null) {
+    name = element.value;
+}
+else {
+    name = null;
+	//alert("empty str");
+}
+	
+//alert(name);
+if(name != null){
+	
+	const xhttp = new XMLHttpRequest();
+		xhttp.onload = function() {
+		  var result = this.responseText;
+		//alert(result);
+		
+		if(result == 1)
+		{
+			alert("The SLA/AMC for Account "+name+" is expired..");
+			
+			document.getElementById('account_name').value='';
+			document.getElementById('account_id').value='';
+			
+			return false;
+			
+		}
+		
+			
+	  }	  
+	  xhttp.open("GET", "include/javascript/takeContactsNameSlaDate.php?name="+name);
+	  xhttp.send();
+	}	
+}
+getcheckContactsName();
+const sauravi = setInterval(function() {getcheckContactsName()}, 3000);
+	
+
+function getcheckContactsNameSupport(){
+var nameVal,
+element = document.getElementById('account_name');
+if (element != null) {
+    nameVal = element.value;
+}
+else {
+    nameVal = null;
+	//alert("empty str");
+}
+	
+//alert(nameVal);
+if(nameVal != null){
+	
+	const xhttp = new XMLHttpRequest();
+		xhttp.onload = function() {
+		  var result = this.responseText;
+		//alert(result);
+		
+		if(result == 1)
+		{
+			alert("The SLA/AMC for Account "+nameVal+" is not in support..");
+			
+			document.getElementById('account_name').value='';
+			document.getElementById('account_id').value='';
+			
+			return false;
+			
+		}
+		
+			
+	  }	  
+	  xhttp.open("GET", "include/javascript/takeContactsNameSlaSupport.php?name="+name);
+	  xhttp.send();
+	}	
+}
+getcheckContactsNameSupport();
+const sauravi1 = setInterval(function() {getcheckContactsNameSupport()}, 3000);
+
+
+
+function getcheckContactsNameSlaTicket(){
+var nameaccount,
+element = document.getElementById('account_name');
+if (element != null) {
+    nameaccount = element.value;
+}
+else {
+    nameaccount = null;
+	//alert("empty str");
+}
+	
+//alert(nameaccount);
+if(nameaccount != null){
+	
+	const xhttp1 = new XMLHttpRequest();
+		xhttp1.onload = function() {
+		  var result = this.responseText;
+		//alert(result);
+		
+		if(result == 1)
+		{
+			alert("The SLA/AMC for Account "+nameaccount+" ticket limit is exceeded..");
+			
+			document.getElementById('account_name').value='';
+			document.getElementById('account_id').value='';
+			
+			return false;
+			
+		}
+		
+			
+	  }	  
+	  xhttp1.open("GET", "include/javascript/takeContactsNameSlaTicket.php?name="+nameaccount);
+	  xhttp1.send();
+	}	
+}
+getcheckContactsNameSlaTicket();
+const sauravi2 = setInterval(function() {getcheckContactsNameSlaTicket()}, 3000);
+
+
+
+function getcheckContactsNameempty(){
+var nameaccountem,
+element = document.getElementById('account_name');
+if (element != null) {
+    nameaccountem = element.value;
+}
+else {
+    nameaccountem = null;
+	//alert("empty str");
+}
+	
+//alert(nameaccountem);
+if(nameaccountem != null){
+	
+	const xhttp2 = new XMLHttpRequest();
+		xhttp2.onload = function() {
+		  var result = this.responseText;
+		//alert(result);
+		
+		if(result == 1)
+		{
+			alert("Please add all Reuired information related to this "+nameaccountem+" account. To create the ticket..");
+			
+			document.getElementById('account_name').value='';
+			document.getElementById('account_id').value='';
+			
+			return false;
+			
+		}
+		
+			
+	  }	  
+	  xhttp2.open("GET", "include/javascript/takeContactsNameSlaEmpty.php?name="+nameaccountem);
+	  xhttp2.send();
+	}	
+}
+getcheckContactsNameempty();
+const sauravi3 = setInterval(function() {getcheckContactsNameempty()}, 3000);
+
+
+function getcheckendDate(){
+var endDate,
+element = document.getElementById('end_date_c_date');
+
+if (element != null) {
+	
+    endDate = element.value;
+	
+}
+else {
+    endDate = null;
+	//alert("empty str");
+}
+//alert(endDate);
+if(endDate != null){
+	
+	//alert(endDate);
+	var date_entered = document.getElementById('date_entered').innerHTML;
+		//alert(date_entered);
+	var endHR = document.getElementById('end_date_c_hours').value;
+    var endMin = document.getElementById('end_date_c_minutes').value;
+	var endmeridiem = document.getElementById('end_date_c_meridiem').value;	
+		
+	var EndDT = endDate+" "+endHR+":"+endMin+" "+endmeridiem;
+	
+	const xhttp2 = new XMLHttpRequest();
+		xhttp2.onload = function() {
+		  var result = this.responseText;
+		//alert(result);
+		
+		if(result == 1)
+		{
+			alert('The end date should not be less than or equal to the ticket created date..');
+			
+			   document.getElementById('end_date_c_date').value='';
+			   document.getElementById('end_date_c_hours').value='';
+			   document.getElementById('end_date_c_minutes').value='';
+			   document.getElementById('end_date_c_meridiem').value='';
+			   document.getElementById('tat_time_c').value ='';
+					
+			return false;
+			
+		}
+		else if(result == 2)
+		{
+			alert("The ticket can't be close before 15 mins...");
+			
+			   document.getElementById('end_date_c_date').value='';
+			   document.getElementById('end_date_c_hours').value='';
+			   document.getElementById('end_date_c_minutes').value='';
+			   document.getElementById('end_date_c_meridiem').value='';
+			   document.getElementById('tat_time_c').value = '';
+					
+			return false;
+		}
+		else 
+		{
+			document.getElementById('tat_time_c').value = result;
+			
+		}
+		
+			
+	  }	  
+	  xhttp2.open("GET", "include/javascript/takeEndDateValidation.php?endDate="+EndDT+"&date_entered="+date_entered);
+	  xhttp2.send();
+	
+	}	
+}
+getcheckendDate();
+const sauravi4 = setInterval(function() {getcheckendDate()}, 22000);
+
+
+function getcheck_TAT(){
+var endDate,
+element = document.getElementById('end_date_c_date');
+
+if (element != null) {
+    endDate = element.value;
+}
+else {
+    endDate = null;
+	//alert("empty str");
+	}
+//alert(endDate);
+if(endDate != null){
+	
+	//alert(endDate);
+	var date_entered = document.getElementById('date_entered').innerHTML;
+		//alert(date_entered);
+	var endHR = document.getElementById('end_date_c_hours').value;
+    var endMin = document.getElementById('end_date_c_minutes').value;
+	var endmeridiem = document.getElementById('end_date_c_meridiem').value;	
+		
+	var EndDT = endDate+" "+endHR+":"+endMin+" "+endmeridiem;
+	
+	const xhttp2 = new XMLHttpRequest();
+		xhttp2.onload = function() {
+		  var result = this.responseText;
+		//alert(result);
+		//document.getElementById('tat_time_c').value = result;
+		
+		if(result == 1)
+		{
+			alert("The ticket can't be close before 15 mins...");
+			
+			   document.getElementById('end_date_c_date').value='';
+			   document.getElementById('end_date_c_hours').value='';
+			   document.getElementById('end_date_c_minutes').value='';
+			   document.getElementById('end_date_c_meridiem').value='';
+			   document.getElementById('tat_time_c').value = '';
+					
+			return false;
+		}
+		else 
+		{
+			document.getElementById('tat_time_c').value = result;
+			
+		}
+		
+	  }	  
+	  xhttp2.open("GET", "include/javascript/takeTAT_timeUpdate.php?endDate="+EndDT+"&date_entered="+date_entered);
+	  xhttp2.send();
+	
+	}	
+}
+getcheck_TAT();
+const sauravi5 = setInterval(function() {getcheck_TAT()}, 1000);
+
+
+function check_form(EditView)
+    {
+	
+		var state = document.getElementById('state').value;
+		//alert(state);
+		var end_date = document.getElementById('end_date_c_date').value;
+		
+		if((state == "Resolved" || state == "Closed")&& (end_date == ""))
+		{
+			alert("Please Select End Date and Time.");
+			return false;
+		}
+		else
+		{
+			return validate_form(EditView, '');
+        }	
+	
+    }
+
+function checkCaseValidation(){
+	
+    var value = document.getElementById("state").value;
+	var case_numberValCheck = document.getElementById('case_number').innerHTML;
+	var date_enteredValCheck = document.getElementById('date_entered').innerHTML;
+	var resolutionVal = document.getElementById('resolution').value;
+	var lengthVal = resolutionVal.length;
+	
+	if((value == 'Resolved' || value == 'Closed')&& (case_numberValCheck == "" && date_enteredValCheck == "")){
+		
+        alert("Before creating new cases, the case should not be closed. First create a new case and then close it.");
+		document.getElementById('state').value = '';
+		
+    }
+	else if((value == "Resolved" || value == "Closed")&& (lengthVal < '30')){
+		
+		alert("Plaese Add Resolution first then select state and Resolution should more than 30 char..");
+		document.getElementById('state').value = "";
+		
+	}
+	
+	
+    
+}
+checkCaseValidation();
+
+function checkCaseTypeHC(){
+	
+    var case_type_Val = document.getElementById("case_type_c").value;
+	
+    if(case_type_Val == 'Health_Checkup'){
+		
+        document.getElementById("filename_new").style.display = 'block';
+
+    }
+    else {
+        document.getElementById("filename_new").style.display = 'none';
+    }
+}
+checkCaseTypeHC();
